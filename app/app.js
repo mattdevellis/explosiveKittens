0b5ff1615c44c416e231f0282ef307ece187cbb9
Matthew Devellis <devellism2@yahoo.com>
2017-02-10 00:30:30 +0000

    Began Vue.js Project

--- app/app.js
+++ app/app.js
@@ -1,15 +1,5 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var path = require('path');
-var expressValidator = require('express-validator');
+import Vue from 'vue'
 
-
-var app = express();
-
-app.get('/', function (req, res) {
-  res.send('Hello World!');
-});
-
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
-});
+new Vue({
+    el:
+})
