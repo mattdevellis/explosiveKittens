148c1651af43f68059d6abdda5a1e1699ca014c1
Matthew Devellis <devellism2@yahoo.com>
2017-03-10 02:59:42 +0000

    Fixed JS/began scss

--- app/resources/js/app.js
+++ app/resources/js/app.js
@@ -3,35 +3,38 @@ let Vue = require('vue');
 
 window.$ = window.jQuery = require('jquery');
 
-window.$ = window.Bootstrap_SCSS = require('bootstrap-sass');
+require('bootstrap-sass');
 
-window.$ = window.Animate = require('animate');
+require('scrollreveal');
+
+window.animate = require('animate');
 
 window.Event = new Vue();
+
+window.sr = require('scrollreveal')();
 
 //Vue.component('navigation', require('./Navigation.vue'));
 
 $(window).ready(function(){
-//     $("a.scroll[href^='#']").on('click', function(e) {
-// 		e.preventDefault();
-// 		var hash = this.hash;
-// 		$('html, body').animate({ scrollTop: $(this.hash).offset().top}, 1000, function(){window.location.hash = hash;});
-// 	});
 	
     $('.navbar a').click(function(e) {
         e.preventDefault();
         $('html, body').animate({
-            scrollTop: $( $.attr(this, 'href')).offset().top-60
+            scrollTop: $( $.attr(this, 'href')).offset().top - $('.navbar ').outerHeight(true)
         }, 500);
     });
     
-     $('.h1.scrollpoint.sp-effect3').fadeIn(2000);
-     $('.h2.scrollpoint.sp-effect3').fadeIn(4000);
+    sr.reveal('.headline', {
+        reset: true,
+        duration: 1000,
+        delay: 50
+    });
     
-    if($(window).width() > 767) {
-		$('.scrollpoint.sp-effect1').waypoint(function(){$(this).toggleClass('active');$(this).toggleClass('animated fadeInLeft');},{offset:'90%'});
-		$('.scrollpoint.sp-effect2').waypoint(function(){$(this).toggleClass('active');$(this).toggleClass('animated fadeInRight');},{offset:'90%'});
-		$('.scrollpoint.sp-effect3').waypoint(function(){$(this).toggleClass('active');$(this).toggleClass('animated fadeInDown');},{offset:'90%'});
-		$('.scrollpoint.sp-effect4').waypoint(function(){$(this).toggleClass('active');$(this).toggleClass('animated fadeIn');},{offset:'70%'});
-    }
+    sr.reveal('.featurette', {
+        reset: true, 
+        duration: 1000,
+        viewOffset: { top: 60 },
+        delay: 50
+    });
+    
 });
--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,6 @@
 const { mix } = require('laravel-mix');
 
 mix.js('app/resources/js/app.js', 'app/public/js/app.js')
-    .copy('app/resources/sass/one-page-wonder.css', 'app/public/css/one-page-wonder.css');
+.sass('app/resources/sass/app.scss', 'app/public/css/app.css')
+    .copy('app/resources/sass/one-page-wonder.css', 'app/public/css/one-page-wonder.css')
+    .copy('app/resources/sass/animate.css', 'app/public/css/animate.css')
